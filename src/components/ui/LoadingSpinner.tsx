6122abc9aa291880985f0555249e5234fce7708d
Dushime Aline Denise <alinedushdenyse@gmail.com>
2026-04-07 19:52:02 +0200

    Create reusable UI components (Button, Input, Modal, Spinner, ErrorMessage)

---
+++ src/components/ui/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+const LoadingSpinner = () => (
+  <div className="flex items-center justify-center min-h-[200px]">
+    <div className="w-10 h-10 border-4 border-purple-500 border-t-transparent rounded-full animate-spin" />
+  </div>
+);
+
+export default LoadingSpinner;
