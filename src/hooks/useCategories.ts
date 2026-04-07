c007ff6563c246c94b8b2986bdac824c66a83d38
Dushime Aline Denise <alinedushdenyse@gmail.com>
2026-04-07 19:47:26 +0200

    Create product and category hooks using React Query

---
+++ src/hooks/useCategories.ts
@@ -0,0 +1,30 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { getAllCategories, createCategory, updateCategory, deleteCategory } from '../api/categoryService';
+
+export const useCategories = () =>
+  useQuery({ queryKey: ['categories'], queryFn: getAllCategories, staleTime: 1000 * 60 * 10 });
+
+export const useCreateCategory = () => {
+  const qc = useQueryClient();
+  return useMutation({
+    mutationFn: createCategory,
+    onSuccess: () => qc.invalidateQueries({ queryKey: ['categories'] }),
+  });
+};
+
+export const useUpdateCategory = () => {
+  const qc = useQueryClient();
+  return useMutation({
+    mutationFn: ({ id, payload }: { id: string; payload: { name: string; image: string } }) =>
+      updateCategory(id, payload),
+    onSuccess: () => qc.invalidateQueries({ queryKey: ['categories'] }),
+  });
+};
+
+export const useDeleteCategory = () => {
+  const qc = useQueryClient();
+  return useMutation({
+    mutationFn: deleteCategory,
+    onSuccess: () => qc.invalidateQueries({ queryKey: ['categories'] }),
+  });
+};
