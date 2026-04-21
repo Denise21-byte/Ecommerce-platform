c880844a8e59353dc1431ae7d52b6ccb46b14ba6
Dushime Aline Denise <alinedushdenyse@gmail.com>
2026-04-21 22:29:55 +0200

    changes in layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,8 +1,8 @@
-import { ReactNode } from 'react';
+import { type ReactNode } from 'react';
 import Navbar from './Navbar';
 
 const Layout = ({ children }: { children: ReactNode }) => (
-  <div className="min-h-screen bg-dark-900">
+  <div className="min-h-screen" style={{ backgroundColor: '#0a0a0f' }}>
     <Navbar />
     <main className="pt-16 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       {children}
